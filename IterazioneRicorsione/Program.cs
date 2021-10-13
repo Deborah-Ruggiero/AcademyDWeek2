fcc8cdda31343cfe2f3dc32ee64636422296b344
Renata Carriero <Renata.Carriero@icubed.it>
2021-10-13 15:51:08 +0200

    Fattoriale + Interessi

--- IterazioneRicorsione/Program.cs
+++ IterazioneRicorsione/Program.cs
@@ -9,8 +9,48 @@ namespace IterazioneRicorsione
             //SommaPrimiNnumeri();
             //Fibonacci();
 
-            
+            Fattoriale();
 
+
+        }
+
+        private static void Fattoriale()
+        {
+            int N = 4;
+            Console.WriteLine("Calcolare il fattoriale di un numero N");
+            //Iterazione
+            int risultatoFattoriale = CalcolaFattoriale(N);
+            Console.WriteLine($"Il fattoriale di {N} (calcolato con iterazione) è uguale a {risultatoFattoriale}");
+
+            //Ricorsione
+            int risultatoFattorialeRic = CalcolaFattorialeRicorsione(N);
+            Console.WriteLine($"Il fattoriale di {N} (calcolato con ricorsione) è uguale a {risultatoFattorialeRic}");
+        }
+
+        private static int CalcolaFattorialeRicorsione(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * CalcolaFattorialeRicorsione(n - 1);
+            }
+        }
+
+        private static int CalcolaFattoriale(int n)
+        {
+            int fatt = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fatt = fatt * i;
+            }
+            //for (int i = N; i > 0; i--)
+            //{
+            //    fatt = fatt * i;
+            //}
+            return fatt;
         }
 
         private static void Fibonacci()
@@ -28,7 +68,6 @@ namespace IterazioneRicorsione
 
         private static int FibonacciRicorsione(int n) 
         {
-
             if(n==0 || n==1)
             {
                 return n;
@@ -38,12 +77,9 @@ namespace IterazioneRicorsione
                 return FibonacciRicorsione(n - 1) + FibonacciRicorsione(n - 2);
             }
         }
-
         private static int FibonacciIterazione(int n)
         {
-            
             int[] arrayFibonacci = new int[n];
-
             for (int i = 0; i < n; i++)
             {
                 if (i <= 1)
