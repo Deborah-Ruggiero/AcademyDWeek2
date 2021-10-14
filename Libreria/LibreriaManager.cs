75138c4cd4c30f5723045f83c9296aad12731a98
Renata Carriero <Renata.Carriero@icubed.it>
2021-10-14 13:01:52 +0200

    Aggiunto Esercizio Libreria

---
+++ Libreria/LibreriaManager.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria
+{
+    public static class LibreriaManager
+    {
+
+        public static List<Libro> libri = new List<Libro>();
+
+        
+
+    public static void AggiungiDatiProva()
+        {
+            Libro libro1 = new Libro() { 
+                Codice = "L001", 
+                Titolo = "I promessi sposi", 
+                Autore = "Manzoni", 
+                Prezzo = 10,
+                Genere = (Genere)1,
+                DataPubblicazione = new DateTime(2010, 10, 12) 
+            };
+            
+            libri.Add(libro1);
+        }
+
+    public static void AggiungiLibro()
+        {
+            //Chiedo all'utente le informazioni necessarie per poter creare un libro.
+            Libro libro = new Libro(); //un libro "vuoto".
+            Console.WriteLine("Inserisci il codice del libro");
+            libro.Codice=Console.ReadLine();
+            Console.WriteLine("Inserisci il titolo del libro");
+            libro.Titolo=Console.ReadLine();
+            Console.WriteLine("Inserisci l'autore del libro");
+            libro.Autore = Console.ReadLine();            
+            libro.Prezzo = InserisciPrezzo();
+            libro.DataPubblicazione = InserisciDataPubblicazione();
+            libro.Genere = InserisciGenere();
+
+            libri.Add(libro);
+            Console.WriteLine("Libro aggiunto correttamente");
+        }
+
+        private static Genere InserisciGenere()
+        {
+            Console.WriteLine("Inserisci il genere del libro");
+            Console.WriteLine($"Premi {(int)Genere.Narrativa} per il genere {Genere.Narrativa}");
+            Console.WriteLine($"Premi {(int)Genere.Storico} per il genere {Genere.Storico}");
+            Console.WriteLine($"Premi {(int)Genere.Fantasy} per il genere {Genere.Fantasy}");
+            int genere;
+            do
+            {
+                Console.WriteLine("Fai la tua scelta");
+            } while (!(int.TryParse(Console.ReadLine(), out genere) && genere>=1 && genere <=3));
+            return (Genere)genere;
+        }
+
+        private static DateTime InserisciDataPubblicazione()
+        {
+            DateTime data;
+            do
+            {
+                Console.WriteLine("Inserisci la data di pubblicazione del libro");
+            } while (!(DateTime.TryParse(Console.ReadLine(), out data) && data<= DateTime.Today));
+            return data;
+        }
+
+        private static double InserisciPrezzo()
+        {
+            double prezzo;
+            do
+            {
+                Console.WriteLine("Inserisci il prezzo del libro");
+            } while (!(double.TryParse(Console.ReadLine(), out prezzo) && prezzo>0));
+            return prezzo;
+        }
+
+        public static void EliminaLibro()
+        {
+            Console.WriteLine("I libri presenti nella libreria sono:");
+            StampaLibri();
+            Console.WriteLine("Scrivi il codice del libro che vuoi eliminare");
+            string codiceDaRicercare=Console.ReadLine();
+            Libro libroTrovato=CercaLibro(codiceDaRicercare);
+            if (libroTrovato == null)
+            {
+                Console.WriteLine("Libro non trovato. Codice errato!");
+            }
+            else
+            {
+                libri.Remove(libroTrovato);
+                Console.WriteLine("Libro eliminato correttamente!");
+            }
+        }
+
+        private static Libro CercaLibro(string codice)
+        {
+            foreach (var item in libri)
+            {
+                if (item.Codice == codice)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+
+        public static void ModificaLibro()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void StampaLibriDiUnaLista(List<Libro> listaLibri)
+        {
+            if (listaLibri.Count == 0)
+            {
+                Console.WriteLine("Lista vuota");
+            }
+            else
+            {
+                Console.WriteLine("Codice\t\tTitolo\t\tAutore\t\tGenere\t\tPrezzo\t\tDataPubblicazione");
+                foreach (var item in listaLibri)
+                {
+                    Console.WriteLine($"{item.Codice}\t\t{item.Titolo}\t\t{item.Autore}\t\t{item.Genere}\t\t{item.Prezzo}\t\t{item.DataPubblicazione.ToShortDateString()}");
+                }
+            }
+        }
+
+        public static void StampaLibri()
+        {
+            StampaLibriDiUnaLista(libri);
+        }
+
+        public static void FiltraLibriPerGenere()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Libreria/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Libreria
+{
+    class Program
+    {
+
+        /*
+          Scrivere un programma che gestisca una libreria.
+ 
+           Un libro è un'entità che ha
+             Codice
+             Titolo
+             Autore
+             Genere
+             Prezzo
+             DataPubblicazione
+ 
+            Per il genere usare un enum
+ 
+            Sarà possibile inserire un nuovo libro, eliminare un libro, modificare un libro o cercare i libri per genere
+         */
+        static void Main(string[] args)
+        {
+            Menu.Start();
+
+            //Libro libro1 = new Libro();
+            //libro1.Codice = "Cod001";
+            //libro1.Autore = "Manzoni";
+            //libro1.Titolo = "I promessi sposi";
+            //libro1.Prezzo = 12;
+            //libro1.DataPubblicazione = new DateTime(2020,10,12);
+
+            //Console.WriteLine($"{libro1.Autore}");
+
+            //Libro libro2 = new Libro();
+            //libro2.Codice = "Cod002";
+            //libro2.Titolo = "La Divina Commedia";
+            //libro2.Autore = "Dante";
+
+            //List<Libro> libri = new List<Libro>();
+            //Console.WriteLine($"La mia lista contiene {libri.Count}");
+            //libri.Add(libro1);
+            //Console.WriteLine($"La mia lista contiene {libri.Count}");
+            //libri.Add(libro2);
+            //Console.WriteLine($"La mia lista contiene {libri.Count}");
+            //libri.Remove(libro1);
+            //Console.WriteLine($"La mia lista contiene {libri.Count}");
+
+        }
+    }
+}
